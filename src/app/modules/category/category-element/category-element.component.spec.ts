18458a2526fbf673e70d7431e116f5ec164a8888
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2021-10-19 15:28:45 +0300

    cateogory add

---
+++ src/app/modules/category/category-element/category-element.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CategoryElementComponent } from './category-element.component';
+
+describe('CategoryElementComponent', () => {
+  let component: CategoryElementComponent;
+  let fixture: ComponentFixture<CategoryElementComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CategoryElementComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryElementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/category/category.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryElementComponent } from './category-element/category-element.component';
+
+
+
+@NgModule({
+  declarations: [
+    CategoryElementComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoryModule { }
