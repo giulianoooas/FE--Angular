4ab7d0e6242f0bba43cbf4e9e462c4395579b17f
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2021-10-19 17:22:49 +0300

    set a max size for comment length

--- src/app/constants/navbar.constant.ts
+++ src/app/constants/navbar.constant.ts
@@ -4,5 +4,17 @@ export const NAVBAR_CONSTANT: Navbar[] = [
   {
     label: 'Cars',
     url: '/cars',
-  }
+  },
+  {
+    label: 'Create car',
+    url: '/cars/create'
+  },
+  {
+    label: 'Categories',
+    url: '/categories'
+  },
+  {
+    label: 'Create category',
+    url: '/categories/create'
+  },
 ]
