1f62c6c8e1c288effe19dec2d19925dcbba3fc99
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2021-11-29 16:40:16 +0200

    add guards

--- src/app/guards/customer.guard.ts
+++ src/app/guards/customer.guard.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerGuard implements CanActivate {
-  canActivate(
+  public constructor(private authService: AuthService){}
+
+  public canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.getIsCustomer();
   }
-  
+
 }
