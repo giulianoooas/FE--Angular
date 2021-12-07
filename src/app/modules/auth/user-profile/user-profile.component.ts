83fc4632772f7656d0ba99d3c613bcb8bb761bd0
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2021-12-07 22:51:04 +0200

    save user profile

--- src/app/modules/auth/user-profile/user-profile.component.ts
+++ src/app/modules/auth/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -7,12 +8,24 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  public user: User;
+  public logoUrl = './assets/images/user-icon.png';
 
   public constructor(
     private authService: AuthService
   ) { }
 
   public ngOnInit(): void {
+    this.setUser();
+    console.log(this.user);
+
+  }
+
+  private setUser(): void{
+    this.user = this.authService.getUser();
+    if (this.user.logoUrl){
+      this.logoUrl = this.user.logoUrl;
+    }
   }
 
 }
