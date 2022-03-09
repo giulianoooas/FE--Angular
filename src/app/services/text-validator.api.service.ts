ea7d32346f8669ff40b623a6e8c1e50ddf074d1d
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2022-03-09 16:08:56 +0200

    add endpoint to take status of a text from my ml object

---
+++ src/app/services/text-validator.api.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TextValidatorAPI{
+  private readonly endpoint = 'http://127.0.0.1:5000/comment-status';
+
+  public constructor(
+    private readonly http: HttpClient
+  ){}
+
+  public getTextStatus(text: string): Observable<string>{ // 0-> bad, 1 -> good
+    return this.http.post<string>(this.endpoint, {
+      comment: text
+    })
+  }
+}
