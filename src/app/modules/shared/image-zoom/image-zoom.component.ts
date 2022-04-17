28d80c9d2b34e1b0a54d338ea06064ed0389d51b
giulianoooas <62396537+giulianoooas@users.noreply.github.com>
2022-04-17 19:53:47 +0300

    make zoomer user fabric js

--- src/app/modules/shared/image-zoom/image-zoom.component.ts
+++ src/app/modules/shared/image-zoom/image-zoom.component.ts
@@ -1,24 +1,23 @@
-import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import {  Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as fb from  'fabric';
 
 @Component({
   selector: 'app-image-zoom',
   templateUrl: './image-zoom.component.html',
   styleUrls: ['./image-zoom.component.scss']
 })
-export class ImageZoomComponent implements OnInit, AfterViewInit {
+export class ImageZoomComponent implements OnInit {
   imageSrc = "assets/images/GiulianoStore.png";
   public imageSize = 700;
-  private image = document.getElementById('image');
+  private canvas: fb.fabric.Canvas;
+  private image: fb.fabric.Image;
+  private mousePressed = false;
 
   public constructor(
     private dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: {imageUrl:string}){}
 
-
-  public   ngAfterViewInit(): void {
-    this.image = document.getElementById('image');
-  }
 
   public closeDialog(): void{
     this.dialog.closeAll();
@@ -27,28 +26,53 @@ export class ImageZoomComponent implements OnInit, AfterViewInit {
   public ngOnInit(): void{
     if (this.data.imageUrl){
       this.imageSrc = this.data.imageUrl;
+      this.canvas = new fb.fabric.Canvas('canvas',{
+        backgroundImage: this.imageSrc,
+        width: 600,
+        height: 600
+      });
+      this.canvas.defaultCursor = 'grab';
+      this.setCanvasImage();
+      this.setCanvasEvents();
+      this.canvas.renderAll();
     }
+  }
+
+  private setScaleNormal(): void{
+    const scaleX = this.canvas.getWidth() / (this.image.width ?? 1);
+    this.canvas.setZoom(scaleX);
+  }
+
+  private setCanvasImage(): void{
+    fb.fabric.Image.fromURL(this.imageSrc, (image) => {
+      this.image = image;
+      this.setScaleNormal();
+    });
+  }
+
+  private setCanvasEvents(): void{
+    this.canvas.on('mouse:move', (event)=> {
+      if (this.mousePressed){
+        const mEvent = event.e;
+        const delta = new fb.fabric.Point(mEvent.movementX,mEvent.movementY);
+        this.canvas.relativePan(delta);
+      }
+    });
+
+    this.canvas.on('mouse:up', ()=> {
+      this.mousePressed = false;
+    });
+
+    this.canvas.on('mouse:down', ()=> {
+      this.mousePressed = true;
+    });
   }
 
   public zoomIn(): void{
-    if (this.imageSize < 2000){
-      this.imageSize += 100;
-    }
-    this.setSize();
+    this.canvas.setZoom(Math.min(this.canvas.getZoom()+  0.1,3));
   }
 
   public zoomOut(): void{
-    if (this.imageSize > 200){
-      this.imageSize -= 100;
-    }
-    this.setSize();
+    this.canvas.setZoom(Math.max(this.canvas.getZoom() - 0.1,0.1));
   }
-
-  public setSize(): void{
-    if (this.image?.style){
-      this.image.style.width = `${this.imageSize}px`;
-      this.image.style.height = `${this.imageSize}px`;
-    }
-  }
-
 }
